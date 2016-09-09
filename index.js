c27e1ee52f9a4fe8a5c130ff94a1bcf0fa641567
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-09-09 10:49:54 -0400

    message

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 // example from http://howtonode.org/hello-node
 // Load the http module to create an http server.
 const http = require('http')
+const version = process.versions.node
+const message = 'Hello World from Node ' + version + '\n'
 
 // Configure our HTTP server to respond with Hello World to all requests.
 const server = http.createServer(function (request, response) {
   console.log('responding with hello')
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World\n')
+  response.end(message)
 })
 
 const port = process.env.PORT || 1337
 server.listen(port)
 
-const safeEnv = require('safe-env')
-console.log(safeEnv(['NPM_TOKEN', 'DEPLOY_DOMAIN']))
 console.log('Server running at port', port)
+console.log(message)
